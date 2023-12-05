f1ca88b63c0e08dc02229b98fdd026749b25920f
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-12-05 10:40:10 -0500

    feat: create board update and win condition checker

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const gameBoard = () => {
+const gameBoard = ( () => {
 
-    const board = ['', '','', '', '', '', '', '', '']
-    console.log(board.length);
-}
+    const board = ['', '','', '', '', '', '', '', ''];
 
+    const updateBoard = (index, player) => {
+        if (board[index] === '' && (player === 'X' || player === 'O')){
+            board[index] = player;
+
+            updateUI(); // Add in function later
+            console.log(board)
+        }
+    };
+
+    const winConditions = [
+        [0, 1, 2], // rows
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6], // columns
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8], // diagonals
+        [2, 4, 6],
+    ];
+
+    const checkWinner = () => {
+        for (const condition of winConditions) {
+            const [a, b, c] = condition;
+            if (board[a] && board[a] === board[b] && board[b] === board[c]){
+                console.log(`The winner is ${board[a]}`);
+                return board[a];
+            }
+        }
+        console.log('no winner yet')
+        return null;
+    }
+    
+    return { 
+        board,
+        updateBoard,
+        checkWinner,
+    };
+})();

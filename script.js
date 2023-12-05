aebd04767768cca896f108dee7e4879a394e53ee
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-12-05 12:18:38 -0500

    fix: move win conditions to gameLogic now playable in console

--- script.js
+++ script.js
@@ -11,29 +11,6 @@ const gameBoard = ( () => {
         }
     };
 
-    const winConditions = [
-        [0, 1, 2], // rows
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6], // columns
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8], // diagonals
-        [2, 4, 6],
-    ];
-
-    const checkWinner = () => {
-        for (const condition of winConditions) {
-            const [a, b, c] = condition;
-            if (board[a] && board[a] === board[b] && board[b] === board[c]){
-                console.log(`The winner is ${board[a]}`);
-                return board[a];
-            }
-        }
-        console.log('no winner yet')
-        return null;
-    }
-
     const updateUI = () => {
         console.log(board)
     }
@@ -41,7 +18,6 @@ const gameBoard = ( () => {
     return { 
         board,
         updateBoard,
-        checkWinner,
     };
 })();
 
@@ -85,8 +61,32 @@ const gameLogic = ( () => {
         }
     }
 
+    const winConditions = [
+        [0, 1, 2], // rows
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6], // columns
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8], // diagonals
+        [2, 4, 6],
+    ];
+
+    const checkWinner = () => {
+        for (const condition of winConditions) {
+            const [a, b, c] = condition;
+            if (board[a] && board[a] === board[b] && board[b] === board[c]){
+                console.log(`The winner is ${board[a]}`);
+                return board[a];
+            }
+        }
+        console.log('no winner yet')
+        return null;
+    }
+
     return { 
         setBoard,
-        board 
+        board,
+        checkWinner,
     };
 })();
